710d2ed07ff92507998425ac270f3ab892e7599f
SnehaSirohi <105498235+SnehaSirohi@users.noreply.github.com>
2022-08-25 23:11:57 +0530

    Update models.py

--- ewaste/myapp/models.py
+++ ewaste/myapp/models.py
@@ -36,6 +36,26 @@ class Dform(models.Model):
     e_img3 = models.FileField(upload_to='', storage=None, max_length=100)
     date = models.DateField()
 
+class Requestcollector(models.Model):
+    email =models.CharField(max_length = 122)
+    address1=models.CharField(max_length= 122)
+    address2 = models.CharField(max_length = 122)
+    district = models.CharField(max_length = 122)
+    city = models.CharField(max_length = 122)
+    state = models.CharField(max_length = 122)
+    pincode = models.CharField(max_length = 122)
+    contact_no =models.CharField(max_length = 12)
+    ename=models.CharField(max_length=122)
+    EwasteType = models.CharField(max_length =122)
+    size = models.CharField(max_length = 122)
+    weight = models.CharField(max_length=122)
+    quantity = models.CharField(max_length = 122)
+    date_s = models.CharField(max_length = 122)
+    time = models.CharField(max_length = 122)
+    e_img = models.FileField(upload_to='', storage=None, max_length=100)
+    e_img2 = models.FileField(upload_to='', storage=None, max_length=100)
+    e_img3 = models.FileField(upload_to='', storage=None, max_length=100)
+
 class Profile(models.Model):
     address=models.CharField(max_length= 122)
     district = models.CharField(max_length = 122)
