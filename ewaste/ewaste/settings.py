6d5e0d7291343a4f0a32a9131e9df328554dbbd1
AnasSaifeee <105496785+AnasSaifeee@users.noreply.github.com>
2022-08-25 12:19:32 +0530

    Update settings.py

--- ewaste/ewaste/settings.py
+++ ewaste/ewaste/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'ewaste.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME':BASIC_DIR / 'db.sqlite3',
+         'NAME':BASE_DIR / 'db.sqlite3',
        
     }
 }
