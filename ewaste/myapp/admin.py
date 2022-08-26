b6df55b10aa0d7584331ed603967e8c6a58b1c48
abhishekty17 <103133303+abhishekty17@users.noreply.github.com>
2022-08-26 11:40:58 +0530

    Update admin.py

--- ewaste/myapp/admin.py
+++ ewaste/myapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from myapp.models import Dform, Profile
-from myapp.models import extendeduser
-admin.site.register(extendeduser)
+from myapp.models import Contributor,Collector
+admin.site.register(Contributor)
+admin.site.register(Collector)
 admin.site.register(Dform)
 admin.site.register(Profile)
